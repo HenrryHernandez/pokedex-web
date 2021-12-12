8c61407a3d860f8b93d485a95aee670785a7eecc
Henrry Hernandez <jenrruhndz@gmail.com>
2021-12-11 22:43:06 -0700

    style (Navbar): add navbar mock up

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,3 +1,43 @@
+import { NavLink } from "react-router-dom";
+
+import { SearchInput } from "./SearchInput";
+
+import pokemonLogo from "../../assets/logo.png";
+
 export const Navbar = () => {
-  return <div></div>;
+  return (
+    <header>
+      <img className="logo" src={pokemonLogo} />
+
+      <nav className="navbar">
+        <ul className="menu">
+          <li className="menu-item">
+            <NavLink className="navlink" to="#">
+              PokemonHome
+            </NavLink>
+          </li>
+
+          <li className="menu-item">
+            <NavLink className="navlink" to="#">
+              Categories
+            </NavLink>
+            <ul className="sub-menu">
+              <li className="sub-menu-item">
+                <NavLink className="navlink" to="#">
+                  Type
+                </NavLink>
+              </li>
+              <li className="sub-menu-item">
+                <NavLink className="navlink" to="#">
+                  Color
+                </NavLink>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+
+      <SearchInput />
+    </header>
+  );
 };
---
+++ src/components/ui/SearchInput.tsx
@@ -0,0 +1,7 @@
+export const SearchInput = () => {
+  return (
+    <div className="search-bar-container">
+      <input type="text" className="search-bar" />
+    </div>
+  );
+};
