8cf6295044e2002346719a735d132f6332e4e922
Henrry Hernandez <jenrruhndz@gmail.com>
2021-12-17 13:14:17 -0700

    style (PokemonCard): add basic style

--- src/components/pokemons/PokemonCard.tsx
+++ src/components/pokemons/PokemonCard.tsx
@@ -1,3 +1,21 @@
+import pokebola from "../../assets/pokebola.png";
+
 export const PokemonCard = () => {
-  return <div>PokemonCard</div>;
+  return (
+    <div className="card">
+      <div className="upper-card-container">
+        <img src={pokebola} alt="pokebola-blanca" className="pokebola-img" />
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt="pokemon"
+          className="pokemon-img"
+        />
+      </div>
+
+      <div className="lower-card-container">
+        <h3>Pikka</h3>
+        <h4>#12314</h4>
+      </div>
+    </div>
+  );
 };
