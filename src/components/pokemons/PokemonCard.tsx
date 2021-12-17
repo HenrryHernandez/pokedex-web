9e5ffb0d0476ec67facee391cd9012e295be06ea
Henrry Hernandez <jenrruhndz@gmail.com>
2021-12-17 14:26:36 -0700

    feat (PokemonCard): get and show real data

--- src/components/pokemons/PokemonCard.tsx
+++ src/components/pokemons/PokemonCard.tsx
@@ -1,20 +1,22 @@
 import pokebola from "../../assets/pokebola.png";
 
-export const PokemonCard = () => {
+import { BasicPokemonInfo } from "../../interfaces/Pokemon.interface";
+
+interface Props {
+  pokemon: BasicPokemonInfo;
+}
+
+export const PokemonCard = ({ pokemon }: Props) => {
   return (
     <div className="card">
       <div className="upper-card-container">
         <img src={pokebola} alt="pokebola-blanca" className="pokebola-img" />
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-          alt="pokemon"
-          className="pokemon-img"
-        />
+        <img src={pokemon.image} alt="pokemon" className="pokemon-img" />
       </div>
 
       <div className="lower-card-container">
-        <h3>Pikka</h3>
-        <h4>#12314</h4>
+        <h3>{pokemon.name}</h3>
+        <h4>#{pokemon.id}</h4>
       </div>
     </div>
   );
