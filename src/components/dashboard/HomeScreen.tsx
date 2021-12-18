9905a9138d5c14933814e8c8104f38643cd8efd2
Henrry Hernandez <jenrruhndz@gmail.com>
2021-12-18 00:05:17 -0700

    feat (HomeScreen): create HomeScreen to show all pokemons

--- src/components/dashboard/HomeScreen.tsx
+++ src/components/dashboard/HomeScreen.tsx
@@ -1,3 +1,21 @@
+import { useGetPokemon } from "../../hooks/useGetPokemons";
+
+import { PokemonCard } from "../pokemons/PokemonCard";
+
+import { Navbar } from "../ui/Navbar";
+
 export const HomeScreen = () => {
-  return <div>HomeScreen</div>;
+  const { basicPokemonInfoList } = useGetPokemon();
+
+  return (
+    <div className="home-screen-container">
+      <Navbar />
+
+      <div className="pokemon-card-container">
+        {basicPokemonInfoList.map((el) => (
+          <PokemonCard key={el.id} pokemon={el} />
+        ))}
+      </div>
+    </div>
+  );
 };
