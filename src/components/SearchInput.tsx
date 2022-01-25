433412453ed93f0938394647dfd931767b32216e
Henrry Hernandez <jenrruhndz@gmail.com>
2022-01-25 10:17:18 -0700

    feat (SearchInput): implement SearchInputContext

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,7 +1,18 @@
+import { useContext } from "react";
+
+import { SearchInputContext } from "../contexts/SearchInputContext";
+
 export const SearchInput = () => {
+  const { searchInput, setSearchInput } = useContext(SearchInputContext);
+
   return (
     <div className="search-bar-container">
-      <input type="text" className="search-bar" />
+      <input
+        type="text"
+        className="search-bar"
+        value={searchInput}
+        onChange={(el) => setSearchInput(el.target.value)}
+      />
     </div>
   );
 };
